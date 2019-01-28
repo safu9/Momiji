1fb6f3d8a297bc726879e10d1c4a9e40016258d2
safu9 <safu9.dev@gmail.com>
2019-01-28 19:00:22 +0900

    Update translations

--- i18n/en.ts
+++ i18n/en.ts
@@ -1,111 +1,197 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="en">
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="window.ui" line="61"/>
+        <location filename="window.ui" line="279"/>
         <source>File(&amp;F)</source>
         <translation>File(&amp;F)</translation>
     </message>
     <message>
-        <location filename="window.ui" line="72"/>
+        <location filename="window.ui" line="290"/>
         <source>Help(&amp;H)</source>
         <translation>Help(&amp;H)</translation>
     </message>
     <message>
-        <location filename="window.ui" line="78"/>
+        <location filename="window.ui" line="296"/>
         <source>Edit(&amp;E)</source>
         <translation>Edit(&amp;E)</translation>
     </message>
     <message>
-        <location filename="window.ui" line="103"/>
+        <location filename="window.ui" line="338"/>
         <source>Open...</source>
         <translation>Open...</translation>
     </message>
     <message>
-        <location filename="window.ui" line="111"/>
+        <location filename="window.ui" line="346"/>
         <source>Save</source>
         <translation>Save</translation>
     </message>
     <message>
-        <location filename="window.ui" line="119"/>
+        <location filename="window.ui" line="354"/>
         <source>Save As...</source>
         <translation>Save As...</translation>
     </message>
     <message>
-        <location filename="window.ui" line="127"/>
+        <location filename="window.ui" line="362"/>
         <source>Exit</source>
         <translation>Exit</translation>
     </message>
     <message>
-        <location filename="window.ui" line="135"/>
+        <location filename="window.ui" line="370"/>
         <source>Undo</source>
         <translation>Undo</translation>
     </message>
     <message>
-        <location filename="window.ui" line="143"/>
+        <location filename="window.ui" line="378"/>
         <source>Redo</source>
         <translation>Redo</translation>
     </message>
     <message>
-        <location filename="window.ui" line="151"/>
+        <location filename="window.ui" line="386"/>
         <source>Copy</source>
         <translation>Copy</translation>
     </message>
     <message>
-        <location filename="window.ui" line="159"/>
+        <location filename="window.ui" line="394"/>
         <source>Cut</source>
         <translation>Cut</translation>
     </message>
     <message>
-        <location filename="window.ui" line="167"/>
+        <location filename="window.ui" line="402"/>
         <source>Paste</source>
         <translation>Paste</translation>
     </message>
     <message>
-        <location filename="window.ui" line="175"/>
+        <location filename="window.ui" line="479"/>
         <source>Select All</source>
         <translation>Select All</translation>
     </message>
     <message>
-        <location filename="main.py" line="118"/>
+        <location filename="main.py" line="296"/>
         <source>About Momiji</source>
         <translation>About Momiji</translation>
     </message>
     <message>
-        <location filename="window.ui" line="95"/>
+        <location filename="window.ui" line="330"/>
         <source>New File</source>
         <translation>New File</translation>
     </message>
     <message>
-        <location filename="main.py" line="64"/>
+        <location filename="main.py" line="94"/>
         <source>untitled</source>
         <translation>untitled</translation>
     </message>
     <message>
-        <location filename="main.py" line="71"/>
+        <location filename="main.py" line="101"/>
         <source>Confirm</source>
         <translation>Confirm</translation>
     </message>
     <message>
-        <location filename="main.py" line="71"/>
+        <location filename="main.py" line="101"/>
         <source>This file has chages, do you want to save them?</source>
         <translation>This file has changes, do you want to save them?</translation>
     </message>
     <message>
-        <location filename="main.py" line="84"/>
+        <location filename="main.py" line="117"/>
         <source>Open File</source>
         <translation>Open File</translation>
     </message>
     <message>
-        <location filename="main.py" line="103"/>
+        <location filename="main.py" line="149"/>
         <source>Text files (*.txt);;Any files (*)</source>
         <translation>Text files (*.txt);;Any files (*)</translation>
     </message>
     <message>
-        <location filename="main.py" line="103"/>
+        <location filename="main.py" line="149"/>
         <source>Save File</source>
         <translation>Save File</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="418"/>
+        <source>Find</source>
+        <translation>Find</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="125"/>
+        <source>Match Case</source>
+        <translation>Match Case</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="138"/>
+        <source>Whole Words</source>
+        <translation>Whole Words</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="151"/>
+        <source>Use Regex</source>
+        <translation>Use Regex</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="185"/>
+        <source>Find string</source>
+        <translation>Find string</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="442"/>
+        <source>Find All</source>
+        <translation>Find All</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="236"/>
+        <source>Replace string</source>
+        <translation>Replace string</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="447"/>
+        <source>Replace</source>
+        <translation>Replace</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="460"/>
+        <source>Replace All</source>
+        <translation>Replace All</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="308"/>
+        <source>Find(&amp;I)</source>
+        <translation>Find(&amp;I)</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="426"/>
+        <source>Find Next</source>
+        <translation>Find Next</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="434"/>
+        <source>Find Previous</source>
+        <translation>Find Previous</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="455"/>
+        <source>Replace Next</source>
+        <translation>Replace Next</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="468"/>
+        <source>Select Next</source>
+        <translation>Select Next</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="277"/>
+        <source>Not found</source>
+        <translation>Not found</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="226"/>
+        <source>%d found</source>
+        <translation>%d found</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="273"/>
+        <source>%d replaced</source>
+        <translation>%d replaced</translation>
     </message>
 </context>
 </TS>
--- i18n/ja.ts
+++ i18n/ja.ts
@@ -1,111 +1,197 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="ja">
+<!DOCTYPE TS>
+<TS version="2.1" language="ja">
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="window.ui" line="61"/>
+        <location filename="window.ui" line="279"/>
         <source>File(&amp;F)</source>
         <translation>ファイル(&amp;F)</translation>
     </message>
     <message>
-        <location filename="window.ui" line="72"/>
+        <location filename="window.ui" line="290"/>
         <source>Help(&amp;H)</source>
         <translation>ヘルプ(&amp;H)</translation>
     </message>
     <message>
-        <location filename="window.ui" line="78"/>
+        <location filename="window.ui" line="296"/>
         <source>Edit(&amp;E)</source>
         <translation>編集(&amp;E)</translation>
     </message>
     <message>
-        <location filename="window.ui" line="103"/>
+        <location filename="window.ui" line="338"/>
         <source>Open...</source>
         <translation>開く...</translation>
     </message>
     <message>
-        <location filename="window.ui" line="111"/>
+        <location filename="window.ui" line="346"/>
         <source>Save</source>
         <translation>保存</translation>
     </message>
     <message>
-        <location filename="window.ui" line="119"/>
+        <location filename="window.ui" line="354"/>
         <source>Save As...</source>
         <translation>名前を付けて保存...</translation>
     </message>
     <message>
-        <location filename="window.ui" line="127"/>
+        <location filename="window.ui" line="362"/>
         <source>Exit</source>
         <translation>終了</translation>
     </message>
     <message>
-        <location filename="window.ui" line="135"/>
+        <location filename="window.ui" line="370"/>
         <source>Undo</source>
         <translation>元に戻す</translation>
     </message>
     <message>
-        <location filename="window.ui" line="143"/>
+        <location filename="window.ui" line="378"/>
         <source>Redo</source>
         <translation>やり直す</translation>
     </message>
     <message>
-        <location filename="window.ui" line="151"/>
+        <location filename="window.ui" line="386"/>
         <source>Copy</source>
         <translation>コピー</translation>
     </message>
     <message>
-        <location filename="window.ui" line="159"/>
+        <location filename="window.ui" line="394"/>
         <source>Cut</source>
         <translation>カット</translation>
     </message>
     <message>
-        <location filename="window.ui" line="167"/>
+        <location filename="window.ui" line="402"/>
         <source>Paste</source>
         <translation>ペースト</translation>
     </message>
     <message>
-        <location filename="window.ui" line="175"/>
+        <location filename="window.ui" line="479"/>
         <source>Select All</source>
         <translation>すべて選択</translation>
     </message>
     <message>
-        <location filename="main.py" line="118"/>
+        <location filename="main.py" line="296"/>
         <source>About Momiji</source>
         <translation>Momiji について</translation>
     </message>
     <message>
-        <location filename="window.ui" line="95"/>
+        <location filename="window.ui" line="330"/>
         <source>New File</source>
         <translation>新規ファイル</translation>
     </message>
     <message>
-        <location filename="main.py" line="64"/>
+        <location filename="main.py" line="94"/>
         <source>untitled</source>
         <translation>無題</translation>
     </message>
     <message>
-        <location filename="main.py" line="71"/>
+        <location filename="main.py" line="101"/>
         <source>Confirm</source>
         <translation>確認</translation>
     </message>
     <message>
-        <location filename="main.py" line="71"/>
+        <location filename="main.py" line="101"/>
         <source>This file has chages, do you want to save them?</source>
         <translation>このファイルは変更されています。保存しますか？</translation>
     </message>
     <message>
-        <location filename="main.py" line="84"/>
+        <location filename="main.py" line="117"/>
         <source>Open File</source>
         <translation>ファイルを開く</translation>
     </message>
     <message>
-        <location filename="main.py" line="103"/>
+        <location filename="main.py" line="149"/>
         <source>Text files (*.txt);;Any files (*)</source>
         <translation>テキストファイル (*.txt);;すべてのファイル (*)</translation>
     </message>
     <message>
-        <location filename="main.py" line="103"/>
+        <location filename="main.py" line="149"/>
         <source>Save File</source>
         <translation>ファイルを保存</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="418"/>
+        <source>Find</source>
+        <translation>検索</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="125"/>
+        <source>Match Case</source>
+        <translation>大/小文字一致</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="138"/>
+        <source>Whole Words</source>
+        <translation>単語一致</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="151"/>
+        <source>Use Regex</source>
+        <translation>正規表現</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="185"/>
+        <source>Find string</source>
+        <translation>検索文字列</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="442"/>
+        <source>Find All</source>
+        <translation>すべて検索</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="236"/>
+        <source>Replace string</source>
+        <translation>置換文字列</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="447"/>
+        <source>Replace</source>
+        <translation>置換</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="460"/>
+        <source>Replace All</source>
+        <translation>すべて置換</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="308"/>
+        <source>Find(&amp;I)</source>
+        <translation>検索(&amp;I)</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="426"/>
+        <source>Find Next</source>
+        <translation>次を検索</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="434"/>
+        <source>Find Previous</source>
+        <translation>前を検索</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="455"/>
+        <source>Replace Next</source>
+        <translation>次を置換</translation>
+    </message>
+    <message>
+        <location filename="window.ui" line="468"/>
+        <source>Select Next</source>
+        <translation>次を選択</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="277"/>
+        <source>Not found</source>
+        <translation>見つかりませんでした</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="226"/>
+        <source>%d found</source>
+        <translation>%d件</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="273"/>
+        <source>%d replaced</source>
+        <translation>%d件を置換</translation>
     </message>
 </context>
 </TS>
